b53075f24c192d2d8dcb2fdfd49150aed4e18e17
lukaa8588 <onlycloud491@gmail.com>
2026-07-06 15:15:29 +0400

    Initial game upload by Antigravity

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'tractor-v37';
+const CACHE_NAME = 'tractor-v39';
 const ASSETS = [
-  './index.html?v=37',
+  './index.html?v=39',
   './yandex-sdk.js',
   './manifest.json',
   './icon.png'
